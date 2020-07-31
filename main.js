14358a3e3ef20ca57943811dea25a4534c21cc63
xtrp <36894883+xtrp@users.noreply.github.com>
2020-07-31 01:42:08 -0400

    Add under construction message

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function createWindow() {
   window = new BrowserWindow(windowOptions);
 
   // load main HTML file into window with platform info
-  window.loadFile('index.html');
+  window.loadFile('app.html');
 
   // set variable for onblur and onfocus
   updateWindowFocus = () => {
